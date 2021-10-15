2f6d7ccaac417513822246064f7e9cd8cf4c7819
Hung Pham <hungpv151098@gmail.com>
2021-10-15 15:01:32 +0700

    Quick Fix api

--- server/app.js
+++ server/app.js
@@ -61,8 +61,8 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: false, limit: '100mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
